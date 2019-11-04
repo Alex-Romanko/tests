f1510a149308d1037a9776963f349bfaf3901c04
saaha1995 <romanko.aleksandr.a@gmail.com>
2019-11-05 00:03:36 +0300

    functions ready

--- any_tests/dict.py
+++ any_tests/dict.py
@@ -1,4 +1,20 @@
-from working_with_gshits2 import insert_data
+
+
+def data_preparation(sample_name, form_dict):
+    transitional_data_storrage = []
+    transitional_data_storrage.append(sample_name)
+    transitional_data_storrage.extend(form_dict.get(sample_name))
+    return(transitional_data_storrage)
+
+
+def prep_data_to_push(transitional_data_storrage):
+    insert_data = ''
+    for number, value in enumerate(transitional_data_storrage):
+        if number <= len(transitional_data_storrage)-2:
+            insert_data = insert_data + value + ', '
+        else:
+            insert_data = insert_data + value
+    return(insert_data)
 
 
 form_dict = {
@@ -7,30 +23,8 @@ form_dict = {
     "acb": ['ab', 'ab', 'bc']
 }
 
-print(form_dict)
-
-for i, v in form_dict.items():
-    print(i)
-    print(v)
-
 
 samples_from_protocol = sorted(form_dict.keys())
-print(samples_from_protocol)
-
-
-sample_data = form_dict.get("abc")
-
-print('===> sample_data:', sample_data)
-
 for sample_name in samples_from_protocol:
-    transitional_data_storrage = []
-    transitional_data_storrage.append(sample_name)
-    sample_data = form_dict.get(sample_name)
-    transitional_data_storrage.extend(sample_data)
-    insert_data = ''
-    for number, value in enumerate(transitional_data_storrage):
-        if number <= len(transitional_data_storrage)-2:
-            insert_data = insert_data + value + ', '
-        else:
-            insert_data = insert_data + value
-    print(insert_data)
+    insert_data = prep_data_to_push(data_preparation(sample_name, form_dict))  # now we can push or write data on each step of cycle
+    print(insert_data, "===> pushed")
